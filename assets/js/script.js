ebcf076d32171a3dda1ab03e385fc1b540042f8a
Alejandra Valencia <valencialemus.a@gmail.com>
2021-12-05 18:47:21 -0300

    se agregan tooltip se intenta crear el smothScroll pero no funciona. Ver el bug

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $("a").click(function () {
+
+        var gato = this.hash
+
+        $ ("html, body").animate({
+            scrollTo: $(gato).offset().top - 70
+        },
+        800
+        )
+    })
+
+
+})
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
